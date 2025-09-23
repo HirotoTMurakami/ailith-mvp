5905f1b3bbb68e3f0875dfe3dde645bea43adf62
HirotoTMurakami <marksman.gk@gmail.com>
2025-09-23 18:42:42 +0900

    Update:v1.0.25

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Suspense } from 'react'
 import "./globals.css";
 import Header from './components/Header'
 
@@ -26,7 +27,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <Header />
+        <Suspense fallback={<div />}> 
+          <Header />
+        </Suspense>
         {children}
       </body>
     </html>
